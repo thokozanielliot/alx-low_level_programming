043375161485f52d23186ac76ff0783ffb44a6ca
thokozanielliot <thokozanielliot58@gmail.com>
2023-02-17 05:57:23 -0800

    Print all possible combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 0 to 9 with commas and spaces
+ * main - Prints numbers between 0 to 9 with commas and spaces.
  *
  * Return: Always 0 (Success)
  */
@@ -9,10 +9,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 38; i < 48; i++)
+	for (i = 48; i < 58; i++)
 	{
-		putchae(i);
-		if (i != 47)
+		putchar(i);
+		if (i != 57)
 		{
 			putchar(',');
 			putchar(' ');
