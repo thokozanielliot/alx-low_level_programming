bc0e3369c355ce23e1e1d19170570ee625fe557b
thokozanielliot <thokozanielliot58@gmail.com>
2023-02-17 05:50:04 -0800

    Print all signle digit numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -Prints numbers between 0 to 9
+ * main - Prints numbers between 0 to 9.
  *
  * Return: Always 0 (Success)
  */
@@ -9,10 +9,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
-	puthchar('/n');
+	putchar('\n');
 	return (0);
 }
