3ac5c11aa1c9a39e6c77cb0bc1aeba8b7656bb5c
thokozanielliot <thokozanielliot58@gmail.com>
2023-02-17 05:32:41 -0800

    Compiled c files

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,13 +3,14 @@
 #include <stdio.h>
 
 /**
- * main - Determines whether the last digit is greater than 5, is 0 or is less than 6 and not 0
+ * main - Determines either greater than 5, is less than 6, or is 0
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n, l;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
