6c1c1a19b24c7917cb0c1e37e894d0cf762952f4
thokozanielliot <thokozanielliot58@gmail.com>
2023-03-06 03:16:40 -0800

    Fixed to betty style

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _memset - fills memory with a constant byte
  * @s: memory area
@@ -9,9 +10,10 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	for (i=0; i<n; i++)
-		*(s+i) = b;
 
-	return s;
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+
+	return (s);
 }
 
