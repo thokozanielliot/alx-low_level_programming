6a6b87f23751e5b6e1d281ea707eaebe6bd51eb4
thokozanielliot <thokozanielliot58@gmail.com>
2023-03-08 17:18:54 -0800

    Find length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - find length of a string
+ * @s: string
+ * Return n: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return ( 1 + _strlen_recursion(s + 1));
+	else
+		return (0);
+}
