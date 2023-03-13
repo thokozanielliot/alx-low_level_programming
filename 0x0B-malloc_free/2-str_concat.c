97b937428a933ce666dba00de577fd914ba50852
thokozanielliot <thokozanielliot58@gmail.com>
2023-03-13 14:29:11 -0700

    Fixed to Betty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,7 +21,7 @@ char *str_concat(char *s1, char *s2)
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-	str = malloc(sizeof(char) * (i + j +1));
+	str = malloc(sizeof(char) * (i + j + 1));
 
 	if (str == NULL)
 		return (NULL);
