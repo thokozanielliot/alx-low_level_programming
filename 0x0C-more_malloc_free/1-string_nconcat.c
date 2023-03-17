b5e5c1be1ac95aeb02631384823e928a980af383
thokozanielliot <thokozanielliot58@gmail.com>
2023-03-16 17:03:26 -0700

    Error: exit status 0 instead f 98

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-
 /**
- * string_nconcat - concatenate 2 strings
- * @s1: First string
- * @s2: Second string
- * @n: number of bytes
- * Return: pointer to newly allocated space memory
+ * malloc_checked - allocates memory.
+ * @b: amount of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
  */
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *malloc_checked(unsigned int b)
 {
-	char *ptr;
-	unsigned int s2_len, i, j, l, k;
+	char *p;
 
-	if (s1 == NULL && s2 == NULL)
+	p = malloc(b);
+	if (p == NULL)
 	{
-		s1 = "";
-		s2 = "";
+		free(p);
+		exit(98);
 	}
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-	s2_len = j;
-	if (n >= s2_len)
-	{
-		j = s2_len;
-	}
-	else
-	{
-		j = n;
-	}
-	ptr = malloc(sizeof(char) * (i + j + 1));
-	if (ptr == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	for (k = 0; k < i; k++)
-		ptr[k] = s1[k];
-	for (l = 0; l <= j; l++, k++)
-		ptr[k] = s2[l];
-
-	return (ptr);
+	return (p);
 }
