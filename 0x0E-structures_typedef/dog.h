2337f1b245098ab30b85610a270a62d60954160f
thokozanielliot <thokozanielliot58@gmail.com>
2023-03-21 03:23:18 -0700

    Fixed to Betty Style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * struct dog - contains dogs name, age and the owner name
+ * @name: Name of dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: A structure that will hold different dogs
+ */
 struct dog 
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
