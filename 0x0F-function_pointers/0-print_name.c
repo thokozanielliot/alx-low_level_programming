6c9e2c3e1e184d39f4f5b76709002ce28c970d43
thokozanielliot <thokozanielliot58@gmail.com>
2023-03-22 09:16:23 -0700

    Update file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints the name
- * @name: name to be printed
- * @f: function pointer
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
  *
- * Return: No return
+ * Return: no return.
  */
 void print_name(char *name, void (*f)(char *))
 {
