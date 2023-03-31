698a8bc9f437c2978d22b44996792d930bd3ec77
thokozanielliot <thokozanielliot58@gmail.com>
2023-03-31 03:02:43 -0700

    Add: Add_node file. Update: Lists header file

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * add_node - add a new node at the beginning of a list_t list
+ * @head: head of linked list
+ * @str: string
+ * Return: address of head
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	size_t nchar;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
+	new->len = nchar;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
