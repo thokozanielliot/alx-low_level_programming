55bd40bb8ce84316411cad454d33b51048720c48
thokozanielliot <thokozanielliot58@gmail.com>
2023-04-04 00:48:21 -0700

    Add: Sum listint file

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * sum_listint - add all the data in a list
+ * @head: Singly Linked list
+ *
+ * Return: Sum
+ * 0 if the list is empty
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	if (head == NULL)
+		return (0);
+	else
+	{
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+	return (sum);
+}
