b78c69e0861c50760f04b8b233f46be7d0096917
thokozanielliot <thokozanielliot58@gmail.com>
2023-05-05 21:52:31 -0700

    Betty Style

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,7 +2,7 @@
 
 /**
  * print_binary - Prints a binary notation of a number.
- * @n: number to be printed in binary.
+ * @num: number to be printed in binary.
  */
 
 void print_binary(unsigned long int num)
