70eed4c12c58f78c31f71eec2629c72cab954269
thokozanielliot <thokozanielliot58@gmail.com>
2023-05-08 16:54:56 -0700

    Fix betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,36 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file & prints it
- * @filename: file name to read
- * @letter: numbers to be read & printed
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
- * Return: Number of letters read and printed
- * 0 if filename is NULL
- * 0 if write fails
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file;
-	ssize_t no_read, no_write;
+	int fd;
+	ssize_t nrd, nwr;
 	char *buf;
 
 	if (!filename)
 		return (0);
-	file = open(filename, O_RDONLY);
-	if (file == -1)
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
 
 	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
 
-	no_read = read(file, buf, letters);
-	no_write = write(STDOUT_FILENO, buf, no_read);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	close(file);
+	close(fd);
+
 	free(buf);
 
-	return (no_write);
+	return (nwr);
 }
